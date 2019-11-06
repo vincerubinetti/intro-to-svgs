750120829655b7ff40c28cd82d024e8301ca74fa
Vincent Rubinetti <vince.rubinetti@gmail.com>
2019-11-06 18:09:38 -0500

    add comments to js and css

--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,4 @@
+// make table of contents list of links from headings with ids
 function makeToc() {
   const headings = document.querySelectorAll('h3[id], h4[id]');
   const toc = document.querySelector('#toc');
@@ -22,6 +23,7 @@ function makeToc() {
   }
 }
 
+// make fixed "jump to table of contents" button
 function makeJumpToToc() {
   const toc = document.querySelector('#toc');
   const jump = document.querySelector('#jump_to_toc');
@@ -41,6 +43,7 @@ function makeJumpToToc() {
   });
 }
 
+// make section anchors on headings with ids
 function makeAnchors() {
   const headings = document.querySelectorAll('h3[id], h4[id]');
   for (const heading of headings) {
@@ -53,6 +56,7 @@ function makeAnchors() {
   }
 }
 
+// when url hash changes
 function onHashChange() {
   const id = window.location.hash.replace('#', '');
   const element = document.getElementById(id);
@@ -62,9 +66,10 @@ function onHashChange() {
   element.setAttribute('data-glow', 'true');
   window.setTimeout(() => element.removeAttribute('data-glow'), 2000);
 }
-
 window.addEventListener('load', onHashChange);
 window.addEventListener('hashchange', onHashChange);
+
+// run scripts when page loaded
 window.onload = function() {
   makeToc();
   makeJumpToToc();
